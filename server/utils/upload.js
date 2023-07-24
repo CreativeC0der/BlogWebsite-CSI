9f529b219f2c75f0a52f348455f177c0088e991c
shrijon <shrijon610@gmail.com>
2023-07-24 20:27:39 +0530

    Committted

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,14 +1,17 @@
 import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
 
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
 const storage = new GridFsStorage({
-    url: `mongodb://user:codeforinterview@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url: `mongodb+srv://${username}:${password}@cluster0.vi1eu1j.mongodb.net/?retryWrites=true&w=majority`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
 
-        if(match.indexOf(file.memeType) === -1) 
-            return`${Date.now()}-blog-${file.originalname}`;
+        if (match.indexOf(file.memeType) === -1)
+            return `${Date.now()}-blog-${file.originalname}`;
 
         return {
             bucketName: "photos",
@@ -17,4 +20,4 @@ const storage = new GridFsStorage({
     }
 });
 
-export default multer({storage}); 
+export default multer({ storage }); 
